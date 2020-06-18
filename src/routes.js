67db558e9e289e0efc79ee69e5a1fb1124ffac87
guilherme munis <gmmunis@hotmail.com>
2020-06-18 17:42:02 -0300

    criação de registro

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = express.Router();
 const ProductControllers = require('./controllers/ProductControllers');
 
 routes.get("/products", ProductControllers.index);
+routes.post("/products", ProductControllers.store);
 
 module.exports = routes;
